def5dbdb43eda7dbbabfc99e960328b8b2cac7cb
@yopigambyok <yopigambyok@gmail.com>
2021-12-23 15:05:25 +0700

    grid view & hero animation

--- lib/detail.dart
+++ lib/detail.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers, unnecessary_new
+
 import 'package:flutter/material.dart';
 
 class Detail extends StatelessWidget {
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+// ignore_for_file: unnecessary_new, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/src/material/scaffold.dart';
-import './detail.dart';
+// import './detail.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: "Side Bar",
-    home: new Home(),
+    title: "GRID dan HERO",
+    home: Home(),
     debugShowCheckedModeBanner: false,
   ));
 }
@@ -21,87 +23,192 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String gambar1 =
-      "https://cdn5.f-cdn.com/ppic/165305021/logo/45880587/KuLJp/profile_logo_IUJMP_d0efebb2c82aa49ca00f954fd1cb2804.png";
-  String gambar2 =
-      "https://upload.wikimedia.org/wikipedia/commons/3/3c/Punctuation_Idem.png";
-  String backup = "";
+  List<Container> daftarSuperHero = [];
 
-  String nama1 = "Alfian Prisma Yopiangga";
-  String nama2 = "Web Petikdua";
-  String backupNama = "";
+  var users = [
+    {"nama": "Alfian Prisma Yopiangga", "gambar": "world-user-01.jpg"},
+    {"nama": "Krisna Wahyu Setiawan", "gambar": "world-user-02.jpg"},
+    {"nama": "Bintang Rezeka Ramadhani", "gambar": "world-user-03.jpg"},
+    {"nama": "Muhamammad Rifqi lutfi", "gambar": "world-user-04.jpg"},
+    {"nama": "Muqorroba Lada Sattar", "gambar": "world-user-05.jpg"},
+    {"nama": "Rosalinda Dwi Vicessa", "gambar": "world-user-06.jpg"},
+    {"nama": "Josepphine Kristianti", "gambar": "world-user-07.jpg"},
+    {"nama": "Muadz Fatullah", "gambar": "world-user-08.jpg"},
+  ];
 
-  String email1 = "yopiangga@email.com";
-  String email2 = "petikdua@email.com";
-  String backupEmail = "";
+  _buatList() async {
+    for (var i = 0; i < users.length; i++) {
+      final user = users[i];
+      final String gambar = user["gambar"].toString();
+      daftarSuperHero.add(
+        new Container(
+            padding: EdgeInsets.all(10),
+            child: new Card(
+              child: Column(
+                children: [
+                  new Hero(
+                    tag: user["nama"].toString(),
+                    child: new Material(
+                      child: new InkWell(
+                        onTap: () =>
+                            Navigator.of(context).push(new MaterialPageRoute(
+                          builder: (BuildContext context) => new Detail(
+                              nama: user["nama"].toString(), gambar: gambar),
+                        )),
+                        child: new Image.asset(
+                          "assets/images/$gambar",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
 
-  void gantiuser() {
-    this.setState(() {
-      backup = gambar1;
-      gambar1 = gambar2;
-      gambar2 = backup;
+                  // new Text(user["nama"].toString(), style: new TextStyle(fontSize: 14.0),),
+                ],
+              ),
+            )),
+      );
+    }
+  }
 
-      backupNama = nama1;
-      nama1 = nama2;
-      nama2 = backupNama;
-
-      backupEmail = email1;
-      email1 = email2;
-      email2 = backupEmail;
-    });
+  @override
+  void initState() {
+    // TODO: implement initState
+    _buatList();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: new Text("Demo Sidebar (DRAWER)"),
-        backgroundColor: Colors.green[500],
+        title: new Text("Super Hero 2",
+            style: new TextStyle(
+              color: Colors.white,
+            )),
       ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: [
-            new UserAccountsDrawerHeader(
-              accountName: new Text(nama1),
-              accountEmail: new Text(email1),
-              // currentAccountPicture: new Image(image: NetworkImage("https://cdn5.f-cdn.com/ppic/165305021/logo/45880587/KuLJp/profile_logo_IUJMP_d0efebb2c82aa49ca00f954fd1cb2804.png")),
-              currentAccountPicture: new GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(new MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          new Detail(nama: nama1, gambar: gambar1)));
-                },
-                child: new CircleAvatar(
-                  backgroundColor: Colors.white,
-                  backgroundImage: new NetworkImage(gambar1),
-                ),
-              ),
-              decoration: new BoxDecoration(
-                  image: new DecorationImage(
-                      image: new NetworkImage(
-                          "https://wallpapercave.com/wp/SlF16Av.jpg"),
-                      fit: BoxFit.cover)),
-              otherAccountsPictures: [
-                new GestureDetector(
-                  onTap: () => gantiuser(),
-                  child: new CircleAvatar(
-                    backgroundImage: new NetworkImage(gambar2),
-                    backgroundColor: Colors.white,
-                  ),
-                )
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: daftarSuperHero,
+      ),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  Detail({this.nama = "", this.gambar = ""});
+
+  final String nama;
+  final String gambar;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new ListView(
+        children: [
+          new Container(
+              height: 240,
+              child: new Hero(
+                  tag: nama,
+                  child: new Material(
+                    child: new InkWell(
+                      child: new Image.asset("assets/images/$gambar", fit: BoxFit.cover,),
+                    ),
+                  ))),
+            new BagianNama(nama: nama),
+            new BagianIcon(),
+            new BagianKeterangan(),
+        ],
+      ),
+    );
+  }
+}
+
+
+class BagianNama extends StatelessWidget {
+
+  BagianNama({this.nama = ""});
+
+  final String nama;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: new EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                new Text(nama, style: new TextStyle(fontSize: 20.0, color: Colors.blue)),
+                new Text("$nama@email.com", style: new TextStyle(fontSize: 17.0, color: Colors.grey)),
               ],
             ),
-            new ListTile(
-              title: new Text("Setting"),
-              trailing: new Icon(Icons.settings),
-            ),
-            new ListTile(
-                title: new Text("Close"), trailing: new Icon(Icons.close)),
-          ],
-        ),
+          ),
+
+          new Row(
+            children: [
+              new Icon(Icons.star, size: 30.0, color: Colors.yellow,),
+              new Text("12")
+            ],
+          )
+        ],
       ),
-      body: new Container(),
+    );
+  }
+}
+
+class BagianIcon extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: new EdgeInsets.all(10),
+      child: Row(
+        children: [
+          Expanded(
+            child: new Column(
+              children: [
+                new Icon(Icons.phone, size: 50, color: Colors.blue,),
+                new Text("Call", style: new TextStyle(fontSize: 18, color: Colors.blue))
+              ],
+            ),
+          ),
+          Expanded(
+            child: new Column(
+              children: [
+                new Icon(Icons.message, size: 50, color: Colors.blue,),
+                new Text("Message", style: new TextStyle(fontSize: 18, color: Colors.blue))
+              ],
+            ),
+          ),
+          Expanded(
+            child: new Column(
+              children: [
+                new Icon(Icons.photo, size: 50, color: Colors.blue,),
+                new Text("Photo", style: new TextStyle(fontSize: 18, color: Colors.blue))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class BagianKeterangan extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: new EdgeInsets.all(10),
+      child: new Card(
+        child: Padding(
+          padding: new EdgeInsets.all(20),
+          child: new Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", style: new TextStyle(fontSize: 18), textAlign: TextAlign.justify,)),
+      ),
     );
   }
 }
